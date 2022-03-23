93f6421f842d7102d03d387bbdacf628007a8d6e
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-03-23 18:57:01 +0000

    Update core.php

--- src/public_html/core.php
+++ src/public_html/core.php
@@ -9,7 +9,7 @@ $endpoint = getenv("ENDPOINT_URL");
 $username = getenv("API_USER");
 $password = getenv("API_PASSWORD");
 $endpoint_id = getenv("ENDPOINT_ID");
-$ignoreContainerNames = getenv("IGNORE_CONTAINERS"); // split by |#|
+$ignoreContainerNames = explode("|#|", getenv("IGNORE_CONTAINERS"));
 
 $token = "";
 
